3511b699fe063aa8b45bcef0225bfd3512124cf4
Chanapol <chanapol@muze.co.th>
2023-12-13 11:41:39 +0700

    [fixed] example input

--- apps/poc/src/app/components/InputSurvey.tsx
+++ apps/poc/src/app/components/InputSurvey.tsx
@@ -41,6 +41,7 @@ export default function InputSurvey() {
     .add('*', async (s) => {
       return 'https://d1pjg4o0tbonat.cloudfront.net/content/dam/toshiba-aem/th/electric-water-boiler/conventional/plk-g33t/gallery2.jpg/jcr:content/renditions/cq5dam.web.5000.5000.jpeg'
     })
+
     .build()
 
   svy.custom.onChoicesLazyLoad
@@ -65,8 +66,33 @@ export default function InputSurvey() {
       const resp = await fetch(url, { method: 'GET' })
       const json = await resp.json()
       return {
-        data: json.countries,
-        totalCount: json.total,
+        data: json?.data?.map((d: any) => ({ value: d.key, text: d.label })) || [],
+        totalCount: json?.data?.length || 0,
+      } as any
+    })
+    .add({ name: 'subdistrict', type: 'dropdown' }, async (_, __) => {
+      const province = _.getValue('province')
+      const district = _.getValue('district')
+
+      _.setValue('zipcode', '')
+      const url = `http://localhost:5100/api/v1/master/addresses/subdistrict?provinceCode=${province}&districtCode=${district}`
+      const resp = await fetch(url, { method: 'GET' })
+      const json = await resp.json()
+      return {
+        data: json?.data?.map((d: any) => ({ value: d.key, text: d.label })) || [],
+        totalCount: json?.data?.length || 0,
+      } as any
+    })
+    .add({ name: 'zipcode' }, async (_, __) => {
+      const province = _.getValue('province')
+      const district = _.getValue('district')
+      const subdistrict = _.getValue('subdistrict')
+      const url = `http://localhost:5100/api/v1/master/addresses/zipcode?provinceCode=${province}&districtCode=${district}&subdistrictCode=${subdistrict}`
+      const resp = await fetch(url, { method: 'GET' })
+      const json = await resp.json()
+      return {
+        data: json?.data?.map((d: any) => ({ value: d.key, text: d.label })) || [],
+        totalCount: json?.data?.length || 0,
       } as any
     })
     .build()
